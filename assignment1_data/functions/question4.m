e70ae239ff5a7e17d56d92738d0e3673c47889a1
Gwent <gwentkrause@gmail.com>
2019-04-28 15:00:59 +0200

    adding question 4 a - d code and the plot for d

--- assignment1_data/functions/question4.m
+++ assignment1_data/functions/question4.m
@@ -1,4 +1,4 @@
-function [] = question4()
+function [result] = question4()
 dice = csvread('wuerfel.csv');
 h = hist(dice);
 s = size(h);
@@ -8,6 +8,30 @@ for x = s(1):s(2)
     y = [y h(x)/su];
 end
 bar(y)
+z = [];
+si = size(y);
+for i = 1:si(2)
+    s = 0; 
+    for j = 1:i
+        s = s + y(j);
+    end
+    z = [z s];
+end
+% bar(z);
+
+rndnumber = unifrnd(0,1);
+result = 0;
+for i = 1:10
+    if z(11 - i) < rndnumber
+        
+        result = 12-i;
+        break
+    end
+    if i == 10
+        result = 1;
+    end
+end
+    
+
 grid on
-sum(y);
 end
