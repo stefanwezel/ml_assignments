0ac0d610d11428639bfc191f6411d2e9959f98dd
Gwent <gwentkrause@gmail.com>
2019-05-14 15:09:29 +0200

    changing q2

--- assignment2_data/functions/question2.m
+++ assignment2_data/functions/question2.m
@@ -6,33 +6,35 @@ mu_seabass = 1;
 sigma_salmon = 0.3;
 mu_salmon = 1.6;
 s = size(fish);
-n = 1;
 % fplot(@(x) normpdf(x,mu_salmon, sigma_salmon),[-8 8])
 barsche = [];
 lachse = [];
 seabass_counter = 0;
+    function [y] = evidence(x)
+        y = normpdf(x,mu_seabass, sigma_seabass) + normpdf(x,mu_salmon, sigma_salmon);
+    end
+
     function [y] = apost(x, omega)
         if omega == 1
-            prob = normpdf(x,mu_seabass, sigma_seabass);
+            prob = normpdf(x,mu_seabass, sigma_seabass)*0.5/evidence(x);
         else
-            prob = normpdf(x,mu_salmon, sigma_salmon);
+            prob = normpdf(x,mu_salmon, sigma_salmon)*0.5/evidence(x);
         end
         y = prob;
     end
 for i = 1:s(1)
     x = fish(i);
-    prob_seabass = normpdf(x,mu_seabass, sigma_seabass)/2;
-    prob_salmon =normpdf(x,mu_salmon, sigma_salmon)/2;
-    aposteriori = prob_seabass * 0.5/(prob_seabass * 0.5 + prob_salmon * 0.5);
-    barsche = [barsche aposteriori];
-    lachse = [lachse 1-aposteriori];
-    if aposteriori > 0.5
-        seabass_counter = seabass_counter +1;
+    apostiori_salmon = apost(x,2);
+    apostiori_seabass = apost(x,1);
+    if apostiori_seabass > apostiori_salmon
+        seabass_counter = seabass_counter + 1;
     end
+    barsche = [barsche apostiori_seabass];
+    lachse = [lachse apostiori_salmon];
 end
 % seabass_counter = 622 / 62.2%
 % salmon_counter = 1000-seabass_counter = 378 / 37.8%
-% scatter(fish, barsche)
+% scatter(fish, lachse)
 % xlim([0.4 2]);
 
 % question 3:
@@ -41,8 +43,8 @@ schnittpunkt = fzero(@(x) normpdf(x,mu_seabass, sigma_seabass)- normpdf(x,mu_sal
 seabass_wrong = integral(@(x) normpdf(x,mu_seabass, sigma_seabass), schnittpunkt(1), Inf);
 salmon_wrong = integral(@(x) normpdf(x,mu_salmon, sigma_salmon), -Inf, schnittpunkt(1));
 
-risk_salmon = seabass_wrong * 0.5
-risk_seabass = salmon_wrong * 1.2
+risk_salmon = seabass_wrong * 0.5;
+risk_seabass = salmon_wrong * 1.2;
     function[y] = cond_risk_salmon(x)
         if x < schnittpunkt
             y = 0;
