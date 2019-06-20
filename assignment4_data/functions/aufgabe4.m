b1c8235c61ee831da1eb5d0fa6d545d3cf09826a
Maria Heitmeier <mabuwu83@gmail.com>
2019-06-20 18:27:58 +0200

    printed plots

--- assignment4_data/functions/aufgabe4.m
+++ assignment4_data/functions/aufgabe4.m
@@ -19,12 +19,7 @@ plot(c2(:, 1), c2(:, 2), 'xr');
 c1 = c1';
 c2 = c2';
 mu = [0 0];
-% for i = 1:100
-%     mu1(1) = mu1(1) + c1(i,1)/100;
-%     mu1(2) = mu1(2) + c1(i,2)/100;
-%     mu2(1) = mu2(1) + c2(i,1)/100;
-%     mu2(2) = mu2(2) + c2(i,2)/100;
-% end
+
 mu1 = sum(c1, 2)/100;
 mu2 = sum(c2, 2)/100;
 mu(1) = (mu1(1) + mu2(1))/2;
@@ -35,8 +30,6 @@ ScatterW2 = 0;
 diff1 = c1 - mu1;
 diff2 = c2 - mu2;
 for i = 1:100
-    %diff1 = [c1(1,i) c1(2, i)] - mu1;
-    %diff2 = [c2(1,i) c2(2,i)] - mu2;
     ScatterW1 = ScatterW1 + diff1(i) * diff1(i)';
     ScatterW2 = ScatterW2 + diff2(i) * diff2(i)';
 end
@@ -45,26 +38,21 @@ ScatterW = ScatterW1 + ScatterW2;
 ScatterB = (mu1 - mu2) * (mu1-mu2)';
 
 proj = inv(ScatterW) * ScatterB;
-proj
-% j = (proj*ScatterB'*proj')/(proj *ScatterW'*proj')
-
-% a = proj(2)/proj(1);
-% plot(proj(1), proj(2), 'xb')
-% plot(0,0,'ob')
 legend()
+
 figure('Name', 'white')
 hold on
-plot1 = c1'*proj
+plot1 = c1'*proj;
 scatter(plot1(:,1)*100, plot1(:,2)*100)
-plot2 = c2'*proj
+plot2 = c2'*proj;
 scatter(plot2(:,1)*100, plot2(:,2)*100)
 plot(c1(1, :), c1(2, :)', 'og'); 
 plot(c2(1, :)', c2(2, :)', 'xr');
-legend({'c1_new', 'c2_new', 'c1_old', 'c2_old'})
+legend({'c1_{new}', 'c2_{new}', 'c1_{old}', 'c2_{old}'})
 hold off
+
 figure('Name', 'white')
 hold on
 scatter(1:100, c1'*proj(:,1))
 scatter(1:100, c2'*proj(:,1))
 hold off
-%fplot(@(x) proj(:,2)'*x)
