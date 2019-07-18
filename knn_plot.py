d840913a6c72e2b6869fc3891e2555625504d44b
wastedsummer <stefan.wezel@student.uni-tuebingen.de>
2019-07-18 15:13:54 +0200

    added 2c

---
+++ knn_plot.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+X = [x for x in range(0,13)]
+
+P = [(2/6)/5, (2/6)/4, (2/6)/3, (2/6)/3,(2/6)/2, (2/6)/1, (2/6)/1,(2/6)/2,(2/6)/1,
+	(2/6)/1,(2/6)/1,(2/6)/2,(2/6)/3]
+
+
+
+data = [2,5,6,8,9,10]
+
+
+plt.plot(X,P)
+plt.plot(data, [0 for x in range(len(data))], 'bo',color='fuchsia')
+plt.grid()
+plt.xlim(0,12)
+plt.ylim(0,0.7)
+plt.xticks(X)
+plt.show()
